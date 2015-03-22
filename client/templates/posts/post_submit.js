4acd2834a6b92977eff6127154383dbca551023b
Yuan Xue <yuanxue68@gmail.com>
2015-03-22 01:26:47 -0400

    user can now submit new posts

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,20 @@
+Template.postSubmit.events({
+	'submit form': function(event){
+		event.preventDefault();
+
+		var post={
+			url: $(event.target).find('[name=url]').val(),
+			title: $(event.target).find('[name=title]').val()
+		};
+
+		Meteor.call('postInsert',post, function(error,result){
+			if(error){
+				return alert(error.reason);
+			}
+			if(result.postExists)
+				alert('This link has already been posted');
+
+			Router.go('postPage', {_id:result._id});
+		});
+	}
+})
