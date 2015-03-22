47b943d36ea3eea8cc08667fda0be3c019aae9d2
Yuan Xue <yuanxue68@gmail.com>
2015-03-22 15:37:24 -0400

    can now view comments

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,5 +7,9 @@ Template.postItem.helpers({
 
 	ownPost:function(){
 		return this.userId===Meteor.userId();
+	},
+
+	commentsCount: function(){
+		return Comments.find({postId:this._id}).count();
 	}
 });
