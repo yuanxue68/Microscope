4b6477f33c15980ddceaeaa20714b4b9f6468f70
Yuan Xue <yuanxue68@gmail.com>
2015-03-22 16:08:05 -0400

    can submit comments

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,9 +7,5 @@ Template.postItem.helpers({
 
 	ownPost:function(){
 		return this.userId===Meteor.userId();
-	},
-
-	commentsCount: function(){
-		return Comments.find({postId:this._id}).count();
 	}
 });
