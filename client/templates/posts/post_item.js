4226308d3a1de168cab9e273939c286756f60df8
Yuan Xue <yuanxue68@gmail.com>
2015-03-23 03:54:08 -0400

    can rank by votes

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,5 +7,20 @@ Template.postItem.helpers({
 
 	ownPost:function(){
 		return this.userId===Meteor.userId();
+	},
+	upvotedClass: function(){
+		var userId= Meteor.userId();
+		if(userId&&!_.include(this.upvoters, userId)){
+			return 'btn-primary upvotable';
+		} else{
+			return 'disabled';
+		}
 	}
 });
+
+Template.postItem.events({
+	'click .upvotable': function(event){
+		event.preventDefault();
+		Meteor.call('upvote',this._id);
+	}
+})
--- server/fixtures.js
+++ server/fixtures.js
@@ -18,7 +18,9 @@ if(Posts.find().count()===0){
 		url: 'http://google.com',
 		description: "Google's search engine ",
     	submitted: new Date(now - 7 * 3600 * 1000),
-   		commentsCount: 2
+   		commentsCount: 2,
+   		upvoters:[],
+   		votes:0
 	});
 
 	Comments.insert({
@@ -44,7 +46,9 @@ if(Posts.find().count()===0){
 	    url: 'http://bing.com',
 	    description: "microsoft's search engine",
 	    submitted: new Date(now - 10 * 3600 * 1000),
-	    commentsCount: 0
+	    commentsCount: 0,
+	    upvoters:[],
+	    votes:0
 	});
 
 	Posts.insert({
@@ -54,7 +58,9 @@ if(Posts.find().count()===0){
 	    url: 'http://yuanxue68.github.io',
 	    description: "Yuan's personal site",
 	    submitted: new Date(now - 12 * 3600 * 1000),
-	    commentsCount: 0
+	    commentsCount: 0,
+	    upvoters:[],
+	    votes:0
 	});
 
 	for (var i = 0; i < 10; i++) {
@@ -65,7 +71,9 @@ if(Posts.find().count()===0){
 	      	url: 'http://google.com/?q=test-' + i,
 	      	description: "description for " + i,
 	      	submitted: new Date(now - i * 3600 * 1000),
-	      	commentsCount: 0
+	      	commentsCount: 0,
+	      	upvoters:[],
+	      	votes:0
 	    });
   	}
 }
