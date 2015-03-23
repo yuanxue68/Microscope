08f4f2889f0ec9f36d88137d87c9829f1304e54f
Yuan Xue <yuanxue68@gmail.com>
2015-03-23 01:29:38 -0400

    can now edit comment

--- client/templates/posts/comment_item.js
+++ client/templates/posts/comment_item.js
@@ -1,5 +1,37 @@
+Template.postSubmit.created=function(){
+	Session.set('editing',false);
+}
+
 Template.commentItem.helpers({
 	submittedText: function (){
 		return this.submitted.toString();
+	},
+
+	ownComment:function(){
+		return this.userId===Meteor.userId();
+	},
+	editingOrNot:function(){
+		if(Session.get('editing')===true)
+			return true;
+		else
+			return false;
 	}
-})
+});
+
+Template.commentItem.events({
+	'click .edit': function(){
+	    Session.set('editing',true);
+	},
+
+	'submit form': function(event){
+		event.preventDefault();
+		Session.set('editing',false);
+		var commentProperties={
+			body: $(".editBox").val(),
+		}
+
+		Meteor.call("commentUpdate",this._id,commentProperties);
+
+	},
+
+});
